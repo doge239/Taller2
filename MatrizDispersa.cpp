c4c158433e5fbb2f70af7662875b52fddf9b2d1b
Nicolás Cortés <nicolas.cortes08@alumnos.ucn.cl>
2025-10-23 15:28:40 -0300

    MatrizDispersa.cpp final

--- MatrizDispersa.cpp
+++ MatrizDispersa.cpp
@@ -310,3 +310,4 @@ MatrizDispersa* MatrizDispersa::multiplicarMatriz(MatrizDispersa* segundaMatriz)
     cout << "Multiplicacion completada. Operaciones realizadas: " << operaciones << endl;
     return matrizResultado;
 }
+//Clase terminada
