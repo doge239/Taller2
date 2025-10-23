9f5ef482b4e5283fef3dfca682178acaf96e3b9c
NicolasCortes10 <166672976+NicolasCortes10@users.noreply.github.com>
2025-10-23 15:38:15 -0300

    MatrizDispersa.h terminado

--- MatrizDispersa.h
+++ MatrizDispersa.h
@@ -3,19 +3,20 @@
 
 class MatrizDispersa {
 private:
-    Nodo* start;  // Puntero al inicio de la lista
+    Nodo* inicio;
 
 public:
     MatrizDispersa();
-    void add(int value, int xPos, int yPos);
-    int get(int xPos, int yPos);
-    void remove(int xPos, int yPos);
-    void valoresAlmacenados();
-    int densidadMatriz();
-    MatrizDispersa* multiplicarMatriz(MatrizDispersa* second);
-    ~MatrizDispersa();
-    
-    // Constructor de copia y operador de asignación (Rule of Three)
     MatrizDispersa(const MatrizDispersa& otra);
-    MatrizDispersa& duplicarMatriz(const MatrizDispersa& otra);
-};
+    MatrizDispersa& operator=(const MatrizDispersa& otra);
+    
+    void add(int value, int xPos, int yPos);
+    int get(int xPos, int yPos) const;  
+    void remove(int xPos, int yPos);
+    void valoresAlmacenados() const;    
+    int densidadMatriz() const;         
+    void obtenerDimensiones(int& maxX, int& maxY) const;  
+    MatrizDispersa* multiplicarMatriz(MatrizDispersa* second) const;  
+    
+    ~MatrizDispersa();
+}
